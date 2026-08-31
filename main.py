ed27c2dfc3de286e830206a4b6bcd3eb18f79776
Zehra <zehragurergenee@gmail.com>
2026-08-31 16:45:45 +0300

    Add continuous menu loop

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
 gorevler = []
 
-print("================================")
-print("       STAJ GÖREV TAKİP")
-print("================================")
+while True:
+    print()
+    print("================================")
+    print("       STAJ GÖREV TAKİP")
+    print("================================")
 
-print("1 - Görevleri göster")
-print("2 - Yeni görev ekle")
-print("3 - Görevi tamamla")
-print("4 - Görev sil")
-print("5 - Çıkış")
+    print("1 - Görevleri göster")
+    print("2 - Yeni görev ekle")
+    print("3 - Görevi tamamla")
+    print("4 - Görev sil")
+    print("5 - Çıkış")
 
-secim = input("Seçiminiz: ")
+    secim = input("Seçiminiz: ")
 
-if secim == "2":
-    gorev = input("Yeni görevi girin: ")
-    gorevler.append(gorev)
-    print("Görev eklendi:", gorev)
+    if secim == "2":
+        gorev = input("Yeni görevi girin: ")
+        gorevler.append(gorev)
+        print("Görev eklendi:", gorev)
+
+    elif secim == "1":
+        print("\n===== GÖREVLER =====")
+
+        if len(gorevler) == 0:
+            print("Henüz görev yok.")
+        else:
+            for i, gorev in enumerate(gorevler, start=1):
+                print(f"{i}. {gorev}")
+
+    elif secim == "5":
+        print("Program kapatılıyor...")
+        break
+
+    else:
+        print("Geçersiz seçim.")
